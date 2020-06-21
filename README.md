b1d56e30e7a3aaa23e9b2b396f9d20095d78d95e
nothile <nothilegugu@gmail.com>
2020-06-21 23:52:22 +0200

    refactored code

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-## This code solution represent the number of lights which will remain lit after.
+## This code solution represent the number of lights which will remain lit after
 
 ### The following pattern
 
 * person walks through the hallway, then switch off every send bulb [2, 4, 6...]
 * second person walks through to switch off every third buld [ 3, 6, 9...] 
+* and the n the pettern continues until it get to the last bulb. 
 
 ### To run this function locally 
 
 * git clone ``` https://github.com/nothil/backend-assessment.git ```
 * `` node app.js``
-* make sure you use the right path.
+* make sure to use the right path.
--- src/app.js
+++ src/app.js
@@ -11,31 +11,31 @@ const  bulbSwitch = function(n){
     }else{
         numberOfPerson = n;
     }
-
+    
     for(let i = 1; i<= bulbs.length; i++ ){
         if(bulbs[i - 1] == undefined){
             bulbs[i -1] = {name:`#${i}`, state:true};
         }
     }
 
-
+  
     for(let j =2; j <= numberOfPerson + 1; j++){
 
-
-        for(let i =1; i <= bulbs.length; i++){
+    
+        for(let i = 1; i <= bulbs.length; i++){
 
             if((i * j) <= bulbs.length){
                 bulbs[(i * j) -1].state = false;
             }
-
-           if((j == numberOfPerson + 1) && (bulbs[i - 1].state == true)){
+             // push the lights to bulbStillOn[] 
+            if((j == numberOfPerson + 1) && (bulbs[i - 1].state == true)){
                 bulbsStillOn.push(bulbs[i - 1]);
            }
         }
     }
-     console.log(`light bulb still on are ` + bulbsStillOn.length);
+      console.log(`light bulb still on are ` + bulbsStillOn.length);
     
 
 }
-// put the number 
+// add a number 
 bulbSwitch(6)
